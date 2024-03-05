6037673365fc001e946bb1ad5f3810b00376e987
Jorge <jpcuentas123@gmail.com>
2024-03-05 06:44:42 -0500

    fix: fix routes

--- src/app.ts
+++ src/app.ts
@@ -19,11 +19,6 @@ app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {})
 
-app.use('/api/v1/', [
-  async (req, res) => {
-    res.json({ message: 'Wompi API Test' })
-  },
-  ...routes,
-])
+app.use('/api/v1/', ...routes)
 
 export default app
